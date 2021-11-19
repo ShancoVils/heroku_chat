fa82bcb9d715a3d00461466a75b4dcbb3f6f3eb6
Shanen <pernens@mail.ru>
2021-11-19 10:05:52 +0300

    six

--- online_chat/asgi.py
+++ online_chat/asgi.py
@@ -8,14 +8,13 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
-
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import chat.routing
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'online_chat.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
