170893f40ad1341f58e97d7efdf88f3a8cd53e60
Shanen <pernens@mail.ru>
2021-11-19 10:54:39 +0300

    nine

--- manage.py
+++ manage.py
@@ -4,19 +4,12 @@ import os
 import sys
 
 
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'online_chat.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+import os
+import django
+from channels.routing import get_default_application
+import channels
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_chat.settings')
 
-if __name__ == '__main__':
-    main()
+django.setup()
+application = get_default_application()
